f65b355397613f8063f0686fe49c4e1255034ead
Alex Lowe <loweman64@outlook.com>
2020-04-28 23:57:45 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Assignment_6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Assignment_6.exe"
+  "Assignment_6.pdb"
+  "CMakeFiles/Assignment_6.dir/pqueue.cpp.o"
+  "CMakeFiles/Assignment_6.dir/testpqueue.cpp.o"
+  "libAssignment_6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pqueue.h
@@ -0,0 +1,24 @@
+#ifndef PQUEUE_H
+#define PQUEUE_H
+
+typedef const char* ItemT;
+typedef double PriorityT;
+typedef void (*ItemPrinterT)(ItemT item);
+typedef void (*PriorityPrinterT)(PriorityT priority);
+
+struct PQCell;
+struct PriorityQueue {
+    PQCell* head;
+    PriorityQueue() {
+      head = NULL;
+    }
+};
+
+void ShowPriorityQueue(const PriorityQueue& q, ItemPrinterT printItem,
+  PriorityPrinterT printPriority);
+
+bool isEmpty(const PriorityQueue& q);
+void insert(PriorityQueue& q, ItemT i, PriorityT p);
+void remove(PriorityQueue& q, ItemT& i, PriorityT& p);
+
+#endif //PQUEUE_H
